9b31b3d45c2d4dc0a53eea94f8fe70418e30103f
Yash <kumaryashcse@gmail.com>
2023-09-13 05:55:11 +0530

    Optimize default extension setup using SStore2

--- src/presets/BaseRouter.sol
+++ src/presets/BaseRouter.sol
@@ -5,8 +5,8 @@ import { Router, IRouter } from "../core/Router.sol";
 import { IRouterState } from "../interface/IRouterState.sol";
 import { IRouterStateGetters } from "../interface/IRouterStateGetters.sol";
 import { ExtensionManager } from "./ExtensionManager.sol";
-import { DefaultExtensionSet } from "./DefaultExtensionSet.sol";
 import { StringSet } from "../lib/StringSet.sol";
+import "lib/sstore2/contracts/SSTORE2.sol";
 
 /// @title BaseRouter
 /// @author thirdweb (https://github.com/thirdweb-dev/dynamic-contracts)
@@ -15,14 +15,22 @@ import { StringSet } from "../lib/StringSet.sol";
 abstract contract BaseRouter is Router, ExtensionManager {
 
     using StringSet for StringSet.Set;
+    mapping(bytes4 => bool) functionMap;
+    mapping(string => bool) extensionMap;
 
     /// @notice The address where the router's default extension set is stored.
     address public immutable defaultExtensions;
     
     /// @notice Initialize the Router with a set of default extensions.
     constructor(Extension[] memory _extensions) {
-        address defaultExtensionsAddr = _extensions.length > 0 ? address(new DefaultExtensionSet(_extensions)) : address(0);
-        defaultExtensions = defaultExtensionsAddr;
+        address pointer;
+        if(_extensions.length > 0) {
+            pointer = SSTORE2.write(abi.encode(_extensions));
+        }
+
+        _validateExtensions(_extensions);
+
+        defaultExtensions = pointer;
     }
 
     /// @notice Initialize the Router with a set of default extensions.
@@ -31,7 +39,8 @@ abstract contract BaseRouter is Router, ExtensionManager {
             return;
         }
         
-        Extension[] memory defaults = IRouterState(defaultExtensions).getAllExtensions();
+        bytes memory data = SSTORE2.read(defaultExtensions);
+        Extension[] memory defaults = abi.decode(data, (Extension[]));
 
         for(uint256 i = 0; i < defaults.length; i += 1) {
 
@@ -56,5 +65,58 @@ abstract contract BaseRouter is Router, ExtensionManager {
     /// @notice Returns the implementation contract address for a given function signature.
     function getImplementationForFunction(bytes4 _functionSelector) public view virtual override returns (address) {
         return getMetadataForFunction(_functionSelector).implementation;
+    }
+
+    /// @dev Adds a new extension to the Router.
+    function _validateExtensions(Extension[] memory _extensions) internal {  
+        uint256 len = _extensions.length;
+        for (uint256 i = 0; i < len; i += 1) {
+            // Check: extension namespace must not already exist.
+            // Check: provided extension namespace must not be empty.
+            // Check: provided extension implementation must be non-zero.
+            // Store: new extension name.
+            _checkExtensionValidity(_extensions[i]);
+
+            uint256 len = _extensions[i].functions.length;
+            for (uint256 j = 0; j < len; j += 1) {
+                _checkFunctionValidity(_extensions[i].functions[j]);
+            }
+        }
+    }
+
+    /// @dev Returns whether a new extension can be added in the given execution context.
+    function _checkExtensionValidity(Extension memory _extension) internal virtual {
+        // Check: provided extension namespace must not be empty.
+        require(bytes(_extension.metadata.name).length > 0, "ExtensionManager: empty name.");
+
+        require(!extensionMap[_extension.metadata.name], "ExtensionManager: extension exists.");
+        extensionMap[_extension.metadata.name] = true;
+
+        // Check: extension implementation must be non-zero.
+        require(_extension.metadata.implementation != address(0), "ExtensionManager: adding extension without implementation.");
+    }
+
+    /// @dev Validates a function in an Extension.
+    function _checkFunctionValidity(ExtensionFunction memory _extFunction) internal {
+        /**
+         *  Note: `bytes4(0)` is the function selector for the `receive` function.
+         *        So, we maintain a special fn selector-signature mismatch check for the `receive` function.
+        **/
+        bool mismatch = false;
+        if(_extFunction.functionSelector == bytes4(0)) {
+            mismatch = keccak256(abi.encode(_extFunction.functionSignature)) != keccak256(abi.encode("receive()"));
+        } else {
+            mismatch = _extFunction.functionSelector !=
+                bytes4(keccak256(abi.encodePacked(_extFunction.functionSignature)));
+        }
+            
+        // Check: function selector and signature must match.
+        require(
+            !mismatch,
+            "ExtensionManager: fn selector and signature mismatch."
+        );
+        // Check: function must not already be mapped to an implementation.
+        require(!functionMap[_extFunction.functionSelector], "ExtensionManager: function exists.");
+        functionMap[_extFunction.functionSelector] = true;
     }
 }
